a4076e3c0a2e0c327c08d091b9aff9860555c905
Your Name <you@example.com>
2024-04-28 17:57:18 +0300

    Failure class created

---
+++ lib/Featurs/Home/data/repos/home_repo_implementation.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:deyarakapp/Featurs/Home/data/Models/home_properties_model/home_properties_model.dart';
+import 'package:deyarakapp/Featurs/Home/data/repos/home_properties_repo.dart';
+import 'package:deyarakapp/core/utils/Errors/Failure.dart';
+import 'package:deyarakapp/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+
+class HomeRepoImpl implements HomePropertiesRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+  @override
+  Future<Either<Failure, List<HomePropertiesModel>>>
+      fetchHomeProperties() async {
+    try {
+      var data = await apiService.get(
+          endPointPath:
+              'properties?fields=price,size,numberOfRooms,location,images,numberOfBathrooms,address');
+      List<HomePropertiesModel> homePropertiesList = [];
+      for (var property in data['data']['data']) {
+        homePropertiesList.add(HomePropertiesModel.fromJson(property));
+      }
+      return right(homePropertiesList);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+}
