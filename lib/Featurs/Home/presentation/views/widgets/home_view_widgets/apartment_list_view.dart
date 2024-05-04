8659b8ddb1131efddf15d1025ae6e5f8c3fd8241
Your Name <you@example.com>
2024-05-04 06:23:54 +0300

    map donee 💃

---
+++ lib/Featurs/Home/presentation/views/widgets/home_view_widgets/apartment_list_view.dart
@@ -0,0 +1,36 @@
+import 'package:deyarakapp/Featurs/Home/presentation/manager/apartment_cubit/apartment_cubit_cubit.dart';
+import 'package:deyarakapp/Featurs/Home/presentation/views/widgets/home_view_widgets/property_item.dart';
+import 'package:deyarakapp/core/widgets/custom_error_widget.dart';
+import 'package:deyarakapp/core/widgets/custom_loading_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ApartmentPropertiesListView extends StatelessWidget {
+  const ApartmentPropertiesListView({
+    super.key,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ApartmentCubitCubit, ApartmentCubitState>(
+      builder: (context, state) {
+        if (state is ApartmentCubitSuccess) {
+          return ListView.builder(
+            itemCount: state.homeProperties.length,
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+            itemBuilder: (context, index) {
+              return PropertyItem(
+                homeModelobject: state.homeProperties[index],
+              );
+            },
+          );
+        }
+        else if (state is ApartmentCubitFailure) {
+          return CustomErrorWidget(errMsg: state.errMsg);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
+    );
+  }}
